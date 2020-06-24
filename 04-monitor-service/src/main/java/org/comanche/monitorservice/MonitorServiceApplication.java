ef3971d22727d7a637a572986a8a9a0bc60a4b08
lishuai <lishuai@muyuanfoods.com>
2020-06-24 15:24:32 +0800

    MonitorServiceApplication

--- 04-monitor-service/src/main/java/org/comanche/monitorservice/MonitorServiceApplication.java
+++ 04-monitor-service/src/main/java/org/comanche/monitorservice/MonitorServiceApplication.java
@@ -2,8 +2,14 @@ package org.comanche.monitorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine
+@EnableHystrixDashboard
+@EnableHystrix
 public class MonitorServiceApplication {
 
     public static void main(String[] args) {
